e75e9cb517a48be994635c2206cbbf47c84b0630
Eric Roberts <ericjasonroberts@gmail.com>
2018-04-08 10:13:52 -0700

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,6 @@ const pass = process.env.DB_PASS || 'arbbot';
 const db = process.env.DB_NAME || 'arbbot';
 const port = process.env.DB_PORT || '5432';
 const host = process.env.DB_HOST || 'localhost';
-// Update with your config settings.
-
 module.exports =  {
   client: 'pg',
   connection: `postgres://${user}:${pass}@${host}:${port}/${db}`,
