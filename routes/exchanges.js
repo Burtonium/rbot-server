085742121acb2b73b754e4c80a598c2f60592a7d
Eric Roberts <ericjasonroberts@gmail.com>
2018-04-30 12:24:32 -0700

    minor changes to exchanges.js

--- routes/exchanges.js
+++ routes/exchanges.js
@@ -2,7 +2,8 @@ const Exchange = require('../models/exchange');
 
 module.exports.fetchOne = async (req, res, next) => {
   const exchange = await Exchange.query()
-    .where( { exchangeId: req.params.exchangeId }).eager('markets.[pair, exchange], exchangeSettings')
+    .where( { exchangeId: req.params.exchangeId })
+    .eager('markets.[pair, exchange], exchangeSettings')
     .modifyEager('exchangeSettings', query => {
       query.where('userId', '=', req.user.id);
     });
@@ -10,7 +11,7 @@ module.exports.fetchOne = async (req, res, next) => {
 };
 
 module.exports.fetchAll = async (req, res, next) => {
-  console.log("fetchAll()");
   const exchanges = await Exchange.query().eager('markets.[pair, exchange]');
+  
   return res.status(200).json(exchanges);
 };
